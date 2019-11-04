725c280d46c1a6e0dcaa36ef65496206e3cbda37
gustavo andres Barrios Araya <gustavo.barrios@sansano.usm.cl>
2019-11-04 19:51:00 -0300

    Se agrega el sistema de manejo de unidades

---
+++ Videojuego/Assets/Scripts/Unit.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Unit : MonoBehaviour
+{
+    public GameObject selector;
+    private bool State;
+    private Animator mAnimator;
+    private NavMeshAgent mNavMeshAgent;
+    private bool mRunning = false;
+
+    void Start()
+    {
+        State = false;
+        SetSelector(false);
+        SelectionManager.unitList.Add(this);
+        mAnimator = GetComponent<Animator>();
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetSelector(bool statement)
+    {
+        selector.SetActive(statement);
+        State = statement;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(State == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    mNavMeshAgent.destination = hit.point;
+                }
+
+            }
+
+            if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+            {
+                mRunning = false;
+            }
+            else
+            {
+                mRunning = true;
+            }
+
+            mAnimator.SetBool("running", mRunning);
+
+        }
+
+    }
+
+
+}
---
+++ Videojuego/Assets/Scripts/UnitSelector.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelector : MonoBehaviour
+{
+    public RectTransform selectorImage;
+    Rect selectionRect;
+    Vector2 startPos;
+    Vector2 endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawRect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DeactivateAllUnits();
+            startPos = Input.mousePosition;
+            
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            endPos = Input.mousePosition;
+            DrawRect();
+
+            // X axis
+            if(Input.mousePosition.x < startPos.x)
+            {
+                selectionRect.xMin = Input.mousePosition.x;
+                selectionRect.xMax = startPos.x;
+            }
+            else
+            {
+                selectionRect.xMin = startPos.x;
+                selectionRect.xMax = Input.mousePosition.x;
+            }
+
+            // Y axis
+            if (Input.mousePosition.y < startPos.y)
+            {
+                selectionRect.yMin = Input.mousePosition.y;
+                selectionRect.yMax = startPos.y;
+            }
+            else
+            {
+                selectionRect.yMin = startPos.y;
+                selectionRect.yMax = Input.mousePosition.y;
+            }
+
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            CheckSelectedUnits();
+            startPos = Vector2.zero;
+            endPos = Vector2.zero;
+            DrawRect();
+        }
+    }
+
+    void DrawRect()
+    {
+        Vector2 boxStart = startPos;
+        Vector2 center = (boxStart + endPos) / 2;
+
+        selectorImage.position = center;
+
+        float sizeX = Mathf.Abs(boxStart.x - endPos.x);
+        float sizeY = Mathf.Abs(boxStart.y - endPos.y);
+
+        selectorImage.sizeDelta = new Vector2(sizeX, sizeY);
+    }
+
+    void CheckSelectedUnits()
+    {
+        foreach (Unit unit in SelectionManager.unitList)
+        {
+            if (selectionRect.Contains(Camera.main.WorldToScreenPoint(unit.transform.position)))
+            {
+                unit.SetSelector(true);
+            }
+        }
+        
+    }
+
+    void DeactivateAllUnits()
+    {
+        foreach(Unit unit in SelectionManager.unitList)
+        {
+            unit.SetSelector(false);
+        }
+    }
+}
